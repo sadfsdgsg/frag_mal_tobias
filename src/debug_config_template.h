b3d6b13216fc9b20173253de6ddd33d0344b00a5
felix <my@email.stfu>
2022-12-27 21:03:43 +0100

    o.O

--- src/debug_config_template.h
+++ src/debug_config_template.h
@@ -1,3 +1,6 @@
 
 #define DISPLAY_DEBUG           //if defined, gives additional debug information of the display components to the display
+
+
+#define SERIAL_DEBUG            //gives additional debug information to serial
 #define DISPLAY_SERIAL_DEBUG    //if defined, gives additional debug information of the display to serial
